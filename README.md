59b53a8df0cd958a9851f1e925cba0c6afd57f77
Vasil Dakov <vasildakov@gmail.com>
2024-01-07 22:19:05 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# design-patterns
-Design Patterns
+# Design Patterns
+
+PHP Design Patterns
+
+[1]: https://kongulov.dev/blog/enhance-your-php-code-with-decorator-design-pattern
+[2]: https://kongulov.dev/blog/mastering-the-strategy-design-pattern-in-php
+[3]: https://kongulov.dev/blog/understanding-the-adapter-design-pattern-in-php
+
+[4]: https://medium.com/javarevisited/strategy-design-pattern-a-smart-way-to-make-decisions-c8f8c37e3aec
+[5]: https://www.neatcode.org/strategy-pattern/
---
+++ src/Behavioral/Strategy/PaymentController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VasilDakov\DesignPatterns\Behavioral\Strategy;
+
+use RuntimeException;
+
+/**
+ * The Context (PaymentService) consumer
+ */
+final class PaymentController
+{
+    public function __construct(private readonly PaymentService $service)
+    {
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    public function paymentAction(PaymentRequest $request): string
+    {
+        $gateway = match($request->gateway) {
+            'PayPal' => new PayPalGateway(),
+            'Stripe' => new StripeGateway(),
+            default  => throw new RuntimeException('Unsupported Payment Gateway'),
+        };
+
+        $this->service->setGateway($gateway);
+
+        return $this->service->process(new Payment($request->amount));
+    }
+}
